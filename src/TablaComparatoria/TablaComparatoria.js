578d614e9ac70388e95b284033eadfdf7e4e7ae5
Mariano Heller <marianofheller@gmail.com>
2017-10-26 12:41:08 -0300

    hotfix de engine + tabla comparatoria con calculo automatico

--- src/TablaComparatoria/TablaComparatoria.js
+++ src/TablaComparatoria/TablaComparatoria.js
@@ -4,62 +4,29 @@ import './TablaComparatoria.css';
 
 
 export default class TablaComparatoria extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            dataTabla: [],
-        }
-    }
-
-
-    /**
-     * Necesito array con valor de aprox por el metodo + error
-     */
-    handleClickCalcular() {
+    
+    calcularDataTable() {
         const { getAllResults, inputs } = this.props;
         const allResults = getAllResults();
         const aproximantes = allResults.map( (objRes) => {
             return config.methods.find( (m) => m.name===objRes.method ).getFormula({ ...objRes.results.params});
         } );
-        const dataTabla = inputs.map( (input, inputIndex) => {
+        return inputs.map( (input, inputIndex) => {
             const aproxRow = aproximantes.map( (aprox) => aprox(input.x) );
             const errorRow = allResults.map( (objRes) => {
                 return objRes.results.errors[inputIndex].cuadratico;
             })
             return [inputIndex, input.x, input.y, ...aproxRow, ...errorRow];
         });
-        this.setState({
-            dataTabla: dataTabla,
-        })
     }
 
     render() {
         const { inputs, decimals } = this.props;
-        const { dataTabla } = this.state;
         return (
         <div>
             <div className="columns" id="tablaComparatoriaContainer">
                 <div className="column">
                     <div className="title is-4">Tabla comparatoria</div>
-                    <div className="has-text-centered is-hidden-mobile">
-                        <button 
-                        className="button is-primary is-pulled-right" 
-                        onClick={this.handleClickCalcular.bind(this)}
-                        >
-                        Calcular todo
-                        </button>
-                    </div>
-                    <div className="has-text-centered is-hidden-tablet">
-                        <button 
-                        className="button is-primary" 
-                        onClick={this.handleClickCalcular.bind(this)}
-                        >
-                        Calcular todo
-                        </button> 
-                    </div>
-
                 </div>
             </div>
                     
@@ -69,7 +36,7 @@ export default class TablaComparatoria extends Component {
                         <tr>
                             <th colSpan={3}>Datos</th>
                             <th colSpan={config.methods.filter((m) => m.enabled).length}>Modelos aprox.</th>
-                            <th colSpan={config.methods.filter((m) => m.enabled).length}>Error</th>
+                            <th colSpan={config.methods.filter((m) => m.enabled).length}>Error cuadrático</th>
                         </tr>
                         <tr>
                             <th>I</th>
@@ -87,7 +54,7 @@ export default class TablaComparatoria extends Component {
                     </thead>
                     <tbody>
                         {/* Seccion datos */}
-                        {dataTabla.map( (rowData,i) => {
+                        {(this.calcularDataTable()).map( (rowData,i) => {
                             return (       
                                 <tr key={`rowSumatoria${i}`} >
                                     { rowData.map( (cellData, ci) => 
