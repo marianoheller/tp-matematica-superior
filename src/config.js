29ab7e18b36db9a444e2a3570e7f857e5d041978
jlsoto85 <jlsoto85@hotmail.com>
2017-11-09 20:35:34 -0300

    agregados y corregidos metodos faltantes (#1)
    * Agregados metodos Exponencial, Hiperbolica y Potencial
    
    * Set theme jekyll-theme-cayman
    
    * Agregada hiperbolica simple, corregido error en hiperbolica

--- src/config.js
+++ src/config.js
@@ -1,11 +1,10 @@
-
 export default  {
     decimals: [ "0", "1", "2", "3", "4", "5" ],
     methods: [
         {
             name: "Recta",
             enabled: true,
-            formulaToShow: "a*x+b",
+            formulaToShow: "a*x+(b)",
             vals: [
                 {
                     name: "X",
@@ -29,7 +28,7 @@ export default  {
                 b: [ "XY", "Y"]
             },
             transformParams: ( paramsArr ) => {
-                return { 
+                return {
                     a: paramsArr[0],
                     b: paramsArr[1]
                 }
@@ -43,7 +42,7 @@ export default  {
         {
             name: "Parabola",
             enabled: true,
-            formulaToShow: "a*x^2 + b*x + c",
+            formulaToShow: "a*x^2 + b*x + (c)",
             vals: [
                 {
                     name: "X",
@@ -79,7 +78,7 @@ export default  {
                 b: [ "YX^2", "XY", "Y"]
             },
             transformParams: ( paramsArr ) => {
-                return { 
+                return {
                     a: paramsArr[0],
                     b: paramsArr[1],
                     c: paramsArr[2]
@@ -93,8 +92,36 @@ export default  {
         },
         {
             name: "Exponencial",
-            enabled: false,
+            enabled: true,
+            vals: [
+                {
+                    name: "X",
+                    calc: ( {x, y} ) => x,
+                },
+                {
+                    name: "Y",
+                    calc: ( {x, y} ) => Math.log(y),
+                },
+                {
+                    name: "X^2",
+                    calc: ( {x, y} ) => Math.pow(x,2),
+                },
+                {
+                    name: "XY",
+                    calc: ( {x, y} ) => x*(Math.log(y)),
+                },
+            ],
+            matrix: {
+                M: [ ["X^2", "X"], ["X", "I"] ],
+                b: [ "XY", "Y"]
+            },
             formulaToShow: "b*e^(a*x)",
+            transformParams: ( paramsArr ) => {
+                return {
+                    a: paramsArr[0],
+                    b: Math.exp(paramsArr[1])
+                }
+            },
             getFormula: ( { a, b }) => {
                 return (x) => {
                     return b*Math.exp(a*x);
@@ -103,23 +130,117 @@ export default  {
         },
         {
             name: "Potencial",
-            enabled: false,
-            formulaToShow: "B*x^A",
+            enabled: true,
+            vals: [
+                {
+                    name: "X",
+                    calc: ( {x, y} ) => Math.log(x),
+                },
+                {
+                    name: "Y",
+                    calc: ( {x, y} ) => Math.log(y),
+                },
+                {
+                    name: "X^2",
+                    calc: ( {x, y} ) => Math.pow(Math.log(x),2),
+                },
+                {
+                    name: "XY",
+                    calc: ( {x, y} ) => Math.log(x)*(Math.log(y)),
+                },
+            ],
+            matrix: {
+                M: [ ["X^2", "X"], ["X", "I"] ],
+                b: [ "XY", "Y"]
+            },
+            formulaToShow: "b*x^a",
+            transformParams: ( paramsArr ) => {
+                return {
+                    a: paramsArr[0],
+                    b: Math.exp(paramsArr[1])
+                }
+            },
             getFormula: ( { a, b }) => {
                 return (x) => {
-                    return b*Math.pow(x, a);
+                    return b*Math.pow(x,a);
                 }
             },
         },
         {
             name: "Hiperbola",
-            enabled: false,
-            formulaToShow: "A/(x+B)",
+            enabled: true,
+            vals: [
+                {
+                    name: "X",
+                    calc: ( {x, y} ) => 1/x,
+                },
+                {
+                    name: "Y",
+                    calc: ( {x, y} ) => 1/y,
+                },
+                {
+                    name: "X^2",
+                    calc: ( {x, y} ) => Math.pow(1/x,2),
+                },
+                {
+                    name: "XY",
+                    calc: ( {x, y} ) => (1/x)*(1/y),
+                },
+            ],
+            matrix: {
+                M: [ ["X^2", "X"], ["X", "I"] ],
+                b: [ "XY", "Y"]
+            },
+            formulaToShow: "a*x/(x+(b))",
+            transformParams: ( paramsArr ) => {
+                return {
+                    a: (1/paramsArr[1]),
+                    b: (1/paramsArr[1])*paramsArr[0]
+                }
+            },
+            getFormula: ( { a, b }) => {
+                return (x) => {
+                    return a*x/(x+b);
+                }
+            },
+        },
+		        {
+            name: "Hiperbola Simple",
+            enabled: true,
+            vals: [
+                {
+                    name: "X",
+                    calc: ( {x, y} ) => x,
+                },
+                {
+                    name: "Y",
+                    calc: ( {x, y} ) => 1/y,
+                },
+                {
+                    name: "X^2",
+                    calc: ( {x, y} ) => Math.pow(x,2),
+                },
+                {
+                    name: "XY",
+                    calc: ( {x, y} ) => (x)*(1/y),
+                },
+            ],
+            matrix: {
+                M: [ ["X^2", "X"], ["X", "I"] ],
+                b: [ "XY", "Y"]
+            },
+            formulaToShow: "a/(x+(b))",
+            transformParams: ( paramsArr ) => {
+                return {
+                    a: 1/paramsArr[0],
+                    b: (1/paramsArr[0])*paramsArr[1]
+                }
+            },
             getFormula: ( { a, b }) => {
                 return (x) => {
                     return a/(x+b);
                 }
             },
-        },
+        }
     ]
 }
